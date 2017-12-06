cde7133dbe8b5d978e3422af7c7493e898da0d83
Anderson Ismael <aicoutos@users.noreply.github.com>
2017-12-06 15:50:18 -0200

    D  example.env M  view/404.php M  view/home/read.php M  view/layout.php

--- view/home/read.php
+++ view/home/read.php
@@ -1,9 +1,9 @@
 <?php
 $data['title']='Guest';
-$messageCreateForm=$View->view(VIEW_DIR.'message/create', [], false);
-$messages=$View->view(VIEW_DIR.'message/list', $data, false);
+$messageCreateForm=$View->view('message/create', [], false);
+$messages=$View->view('message/list', $data, false);
 $data['content']=<<<heredoc
 <h2>Mensagens</h2>
 heredoc;
 $data['content']=$data['content'].$messages.$messageCreateForm;
-$View->view(VIEW_DIR.'layout', $data);
+$View->view('layout', $data);
