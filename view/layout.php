aad8cd9c802f706c2327e42c32a5ac4315974c61
Anderson Ismael <aicoutos@users.noreply.github.com>
2017-12-07 16:49:50 -0200

    M  view/layout.php

--- view/layout.php
+++ view/layout.php
@@ -5,13 +5,6 @@
     <title><?php print $title; ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.rawgit.com/mushape/1k/master/dist/1k.min.css">
-    <style media="screen">
-    @supports (-moz-appearance:meterbar) {
-        textarea{
-            width: calc(100% - 8px);
-        }
-    }
-    </style>
     <script src="https://cdn.rawgit.com/jquerylight/jquerylight/master/dist/jquerylight.min.js"></script>
 </head>
 <body>
