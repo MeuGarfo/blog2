94bd25aadfd2c67b9867f9ef4856503adf3daaaf
Anderson Ismael <aicoutos@users.noreply.github.com>
2017-12-08 17:07:03 -0200

    M  controller/Posts.php

--- controller/Posts.php
+++ controller/Posts.php
@@ -8,12 +8,15 @@ class Posts
 {
     public function get($slug=null)
     {
-        $auth=ROOT.'app/inc/auth.php';
+        /*VARs*/
+        $auth=require_once ROOT.'app/inc/auth.php';
+        $db=require_once ROOT.'db.php';
+        die(var_dump($db));
         $data['user']=$auth->isAuth();
         $view=new View();
         $data['view']=$view;
+        /*CODEs*/
         if (is_null($slug)) {
-            $db=require_once ROOT.'db.php';
             $where=["id[>=]" => 1];
             $data['posts']=$db->select('posts', '*', $where);
             $view->view('read/posts', $data);
