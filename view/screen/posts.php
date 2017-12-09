5d3e4e3a8a5418db950a200f68bda848af4b7bfa
Anderson Ismael <aicoutos@users.noreply.github.com>
2017-12-08 22:44:27 -0200

    M  view/screen/posts.php

--- view/screen/posts.php
+++ view/screen/posts.php
@@ -4,12 +4,12 @@ if (isset($posts) && is_array($posts) && count($posts)>0) {
     print '<ul>';
     foreach ($posts as $post) {
         $postRead='/posts/'.$post['slug'];
-        $postUpdate='javascript:postUpdate("'.$post['id'].'")';
-        $postDelete='javascript:postDelete("'.$post['id'].'")';
+        $postUpdate='javascript:postUpdate(\''.$post['id'].'\');';
+        $postDelete='javascript:postDelete(\''.$post['id'].'\');';
         print '<li>';
         print '<a href="'.$postUpdate.'">Editar</a> |';
-        print '<a href="'.$postDelete.'">Apagar</a> |';
-        print '<a href="'.$postRead.'">'.$post['title'].'</a>';
+        print ' <a href="'.$postDelete.'">Apagar</a> |';
+        print ' <a href="'.$postRead.'">'.$post['title'].'</a>';
         print '</li>';
     }
     print '</ul>';
