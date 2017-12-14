ce3930b6e449594f2dbb777cfb16aa8a69876bea
Anderson Ismael <aicoutos@users.noreply.github.com>
2017-12-14 16:30:04 -0200

    M  asset/css/master.css M  view/layout.php

--- view/layout.php
+++ view/layout.php
@@ -5,9 +5,9 @@
     <title><?php print $title; ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.rawgit.com/mushape/1k/7d47b0f/dist/1k.min.css">
-    <link rel="stylesheet" href="https://cdn.rawgit.com/aicoutos/blog/5a6020c/asset/css/master.css">
+    <link rel="stylesheet" href="https://cdn.rawgit.com/aicoutos/blog/master/asset/css/master.css">
     <script src="https://cdn.rawgit.com/jquerylight/jquerylight/master/dist/jquerylight.min.js"></script>
-<script src="https://cdn.rawgit.com/aicoutos/blog/5a6020c/asset/js/editor.js"></script>
+<script src="https://cdn.rawgit.com/aicoutos/blog/master/asset/js/editor.js"></script>
     <style media="screen">
     .btn-group{
         text-align: center;
