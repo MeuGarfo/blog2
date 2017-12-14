70b3cd092e043c66598d977e6262b914d7956cdb
Anderson Ismael <aicoutos@users.noreply.github.com>
2017-12-14 16:37:28 -0200

    M  asset/css/main.css M  view/layout.php

--- view/layout.php
+++ view/layout.php
@@ -5,7 +5,7 @@
     <title><?php print $title; ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.rawgit.com/mushape/1k/7d47b0f/dist/1k.min.css">
-    <link rel="stylesheet" href="https://cdn.rawgit.com/aicoutos/blog/5551125/asset/css/main.css">
+    <link rel="stylesheet" href="https://cdn.rawgit.com/aicoutos/blog/4c9e7ac/asset/css/main.css">
     <script src="https://cdn.rawgit.com/jquerylight/jquerylight/master/dist/jquerylight.min.js"></script>
 <script src="https://cdn.rawgit.com/aicoutos/blog/master/asset/js/main.js"></script>
     <style media="screen">
