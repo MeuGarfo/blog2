48dfef43b9911f63ad07678797dd8fb4bbc7bc26
Anderson Ismael <aicoutos@users.noreply.github.com>
2017-12-27 10:04:53 -0200

    M  asset/img/searchicon.png M  controller/Files.php M  view/files/create.php M  view/inc/filesCreate.php

--- controller/Files.php
+++ controller/Files.php
@@ -3,17 +3,20 @@ namespace app\controller;
 
 use Basic\View;
 use Basic\Auth;
+use Basic\Upload;
 
 class Files
 {
     public $db;
     public $auth;
     public $view;
+    public $user;
     public function index($method)
     {
         $this->db=require_once ROOT.'db.php';
         $this->auth=new Auth($this->db);
         $this->view=new View();
+        $this->user=$this->auth->isAuth();
         if ($method=='POST') {
             $this->post();
         } else {
@@ -22,7 +25,7 @@ class Files
     }
     public function get()
     {
-        if (isset($_GET['create'])) {
+        if (isset($_GET['create']) && $this->user) {
             $this->getCreate();
         }
     }
@@ -31,13 +34,35 @@ class Files
         /*VARs*/
         $data=[
             'view'=>$this->view,
-            'user'=>$this->auth->isAuth()
+            'user'=>$this->user
         ];
         /*RULEs*/
-        if ($data['user']) {
-            $this->view->out('files/create', $data);
+        $this->view->out('files/create', $data);
+    }
+    public function post()
+    {
+        if (isset($_GET['create']) && $this->user) {
+            $this->postCreate();
+        }
+    }
+    public function postCreate()
+    {
+        $upload=new Upload();
+        $exts=[
+            'gif',
+            'jpg',
+            'pdf',
+            'png'
+        ];
+        $file=$upload->upload('file', $exts);
+        if (isset($file['error'])) {
+            print '<pre>';
+            print_r($file['error']);
         } else {
-            $this->view->redirect('/signin');
+            print 'arquivo enviado com sucesso<br>';
+            $destination=ROOT.'file/'.$file['name'];
+            $upload->move($file['temp'], $destination);
+            print $_ENV['site_url'].'/file/'.$file['name'];
         }
     }
 }
