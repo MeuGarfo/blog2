a9d46f6f516fe5db25fac52c33bafbae2dfbb9e0
Anderson Ismael <aicoutos@users.noreply.github.com>
2017-12-28 16:41:41 -0200

    M  asset/css/main.css M  view/inc/leftAuthPrivate.php M  view/inc/leftAuthPublic.php

--- view/inc/leftAuthPrivate.php
+++ view/inc/leftAuthPrivate.php
@@ -2,5 +2,6 @@
     <li><a href="/">Home</a></li>
     <li><a href="/posts">Posts</a></li>
     <li><a href="/files">Arquivos</a></li>
+    <li><a href="/feed">RSS</a></li>
     <li><a href="/logout?token=<?php print $user['token'] ?>">Logout</a></li>
 </ul>
--- view/inc/leftAuthPublic.php
+++ view/inc/leftAuthPublic.php
@@ -1,1 +1,5 @@
-publico
+<ul class="lista">
+    <li><a href="/">Home</a></li>
+    <li><a href="/signin">Login</a></li>
+    <li><a href="/feed">RSS</a></li>
+</ul>
